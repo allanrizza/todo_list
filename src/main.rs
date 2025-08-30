722760f6c028047cfe31922953bc861aab7035f1
Allan Gilbert Rizza <allanrizza@gmail.com>
2025-08-30 05:19:08 -0300

    some functions

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,55 @@
+use std::io;
 struct Task {
     description: String,
     completed: bool,
 }
 
 fn main() {
+    
     println!("Bem vindo ao TODO list!");
 
     let mut tasks: Vec<Task> = Vec::new();
 
-    tasks.push(Task {
-        description: String::from("Aprender Rust"),
-        completed: false,
-    });
+    loop {
+        println!("\nComandos: list, add <descrição>, done <id>, remove <id>, sair\n");
+        let mut input = String::new();
 
-    tasks.push(Task {
-        description: String::from("construir o projeto todo list"),
-        completed: true,
-    });
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Falha ao ler a entrada do usuário");
 
-    list_tasks(&tasks);
+        let mut parts = input.trim().split_whitespace();
+
+        let command = parts.next().unwrap_or("");
+
+        if command == "sair" {
+            println!("Saindo...");
+            break;
+        } else if command == "list" {
+            list_tasks(&tasks);
+        } else if command == "add" {
+            let description: String = parts.collect::<Vec<&str>>().join(" ");
+            if description.is_empty() {
+                println!("a descrição não pode estar vazia");
+            } else {
+                println!("adicionando tarefa: {}", description);
+                add_task(&mut tasks, description);
+            }
+        } else if command == "done" {
+            if let Some(id_str) = parts.next() {
+                match id_str.parse::<usize>() {
+                    Ok(id) => {
+                        mark_task_done(&mut tasks, id);
+                    }
+                    Err(_) => {
+                        println!("ERRO: O ID fornecido '{}' não é um número válido.", id_str);
+                    }
+                }
+            }
+        } else {
+            println!("comando desconhecido: {}", command);
+        }       
+    }
 }
 
 fn list_tasks(tasks: &[Task]) {
@@ -32,4 +63,22 @@ fn list_tasks(tasks: &[Task]) {
         }
     }
     println!("---------------------------\n");
+}
+
+fn add_task(tasks: &mut Vec<Task>, description: String) {
+    tasks.push(Task {
+        description: description,
+        completed: false,
+    });
+    println!("Tarefa adicionada com sucesso!");
+}
+
+fn mark_task_done(tasks: &mut Vec<Task>, task_id: usize) {
+    let index = task_id - 1;
+
+    if let Some(task) = tasks.get_mut(index) {
+        task.completed = true;
+    } else {
+        println!("ERRO: ID inválido.");
+    }
 }
